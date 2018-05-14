1c27d036a5694f5252f6a4691ea812f29a5e81e6
muuuuuuura <muuuuuuura@gmail.com>
2018-05-14 22:09:46 +0900

    子テーマ追加により修正。

--- wp-content/themes/lionblog-child/functions.php
+++ wp-content/themes/lionblog-child/functions.php
@@ -24,5 +24,13 @@ add_action('wp_head', 'fit_head_child');
 //下記ユーザーカスタマイズエリア
 //////////////////////////////////////////////////
 
-
+// サイトURL/?author=1でのアクセスを404ページにリダイレクト。 2018/5/13
+add_filter( 'author_rewrite_rules', '__return_empty_array' );
+function disable_author_archive() {
+	if( $_GET['author'] || preg_match('#/author/.+#', $_SERVER['REQUEST_URI']) ){
+		wp_redirect( home_url( '/404.php' ) );
+		exit;
+	}
+}
+add_action('init', 'disable_author_archive');
 ?>
--- wp-content/themes/lionblog/functions.php
+++ wp-content/themes/lionblog/functions.php
@@ -6229,13 +6229,4 @@ function fit_dashboard_widget_function() {
 	<a href="http://fit-jp.com/themeinfo/" target="_blank"><img src="http://fit-jp.com/themeinfo/info-img.png" style="max-width: 100%;"></a>';
 }
 
-// サイトURL/?author=1でのアクセスを404ページにリダイレクト。 2018/5/13
-add_filter( 'author_rewrite_rules', '__return_empty_array' );
-function disable_author_archive() {
-	if( $_GET['author'] || preg_match('#/author/.+#', $_SERVER['REQUEST_URI']) ){
-		wp_redirect( home_url( '/404.php' ) );
-		exit;
-	}
-}
-add_action('init', 'disable_author_archive');
 ?>
